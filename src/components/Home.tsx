438b3a2f218aa54011d72a93753d34390d590e53
Stefano Casasola <ste.casasola@gmail.com>
2022-05-19 11:18:59 +0200

    Connects to the socket.io server and sends user

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useEffect, useState } from 'react'
 import {
   Container,
   Row,
@@ -6,16 +7,68 @@ import {
   FormControl,
   ListGroup,
 } from 'react-bootstrap'
+import { io } from 'socket.io-client'
+
+// 1) establishing a connection
+// 2) logging in sending our username and receiving confermation from the server ('loggedin')
+// 3) partecipate to the conversation sending messages
+
+const ADDRESS = 'http://localhost:3030'
+const socket = io(ADDRESS, { transports: ['websocket'] })
+// socket is a REFERENCE to the established connection
 
 const Home = () => {
+  // every time the WS connection happens correctly, the server is going to emit us
+  // an EVENT back: this initialization event is called 'connect'
+  // so let's set up an event listener for listening on the 'connect' event from the server!
+
+  // let's create a state variable for connecting the username input field
+  const [inputUserName, setInputUserName] = useState('')
+
+  // event listeners have to be set up JUST ONCE
+  useEffect(
+    () => {
+      socket.on('connect', () => {
+        console.log('connection with the server established!')
+      })
+
+      // the 'loggedin' event comes from the server when my username has been added
+      // to the online users list correctly
+      socket.on('loggedin', () => {
+        // now I'm officially logged in!
+        console.log(
+          "the username has been sent successfully and now you're logged in!"
+        )
+      })
+    },
+    [
+      // empty pair of brackets here because ALL my event listeners have to be set up
+      // just once!
+    ]
+  )
+
+  const sendUsername = (e: FormEvent) => {
+    e.preventDefault()
+    // now we're about to emit an event to the server, carrying our username
+    socket.emit('setUsername', {
+      username: inputUserName,
+    })
+    // the server code is already set up to listen for a 'setUsername' event
+    // and grab the username property out of its payload
+  }
+
   return (
     <Container fluid className="px-4 my-3">
       <Row style={{ height: '95vh' }}>
         {/* MAIN COLUMN */}
         <Col md={10} className="d-flex flex-column justify-content-between">
           {/* TOP SECTION: USERNAME INPUT FIELD */}
-          <Form>
-            <FormControl placeholder="Insert your username here..." />
+          <Form onSubmit={sendUsername}>
+            <FormControl
+              value={inputUserName}
+              onChange={(e) => setInputUserName(e.target.value)}
+              placeholder="Insert your username here..."
+            />
           </Form>
           {/* MIDDLE SECTION: CHAT HISTORY */}
           <ListGroup>
